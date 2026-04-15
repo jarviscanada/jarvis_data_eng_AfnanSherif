608bf16850e110a79602ed0c28135f588ec1bcac
Afnan Sherif <afnan.sherif@jrvs.ca>
2026-04-15 19:35:11 +0000

    Fixed psql_docker.sh

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -7,14 +7,15 @@ db_password=$3
 
 # Start docker
 # Make sure you understand the double pipe operator
-sudo systemctl status docker || systemctl #todo
+sudo systemctl status docker > /dev/null || sudo systemctl start docker > /dev/null
 
 # Check container status (try the following cmds on terminal)
-docker container inspect jrvs-psql
+docker container inspect jrvs-psql  >/dev/null 2>&1
+
 container_status=$?
 
 # User switch case to handle create|stop|start opetions
-case $cmd in 
+case "$cmd" in 
   create)
   
   # Check if the container is already created
@@ -24,28 +25,43 @@ case $cmd in
 	fi
 
   # Check # of CLI arguments
-  if [ $# -ne 3 ]; then
+  if [ "$container_status" -ne 0 ]; then
     echo 'Create requires username and password'
     exit 1
   fi
   
   # Create container
-	docker volume #todo
+	docker volume create pgdata
   # Start the container
-	docker run #todo
+	    docker run --name $db_username -e DB_PASSWORD=$db_password -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:18-alpine
+
   # Make sure you understand what's `$?`
 	exit $?
 	;;
 
-  start|stop) 
+stop)
   # Check instance status; exit 1 if container has not been created
-  if [ $container_status # ... todo
-  # ... todo
+  if [ $container_status -ne 0 ]; then
+ echo 'container does not exist'
+ exit 1
+  fi
 
   # Start or stop the container
-	docker container $cmd jrvs-psql
+	docker container stop jrvs-psql
 	exit $?
-	;;	
+	;;
+start)
+  if [ $container_status -ne 0 ]; then
+
+ echo 'container does not exist'
+ exit 1
+  fi
+  # Start or stop the container
+        docker container start jrvs-psql
+        exit $?
+        ;;
+
+
   
   *)
 	echo 'Illegal command'
